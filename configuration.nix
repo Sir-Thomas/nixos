2a8ab4fc2d96ee74fa98fe33db644be34f88c265
Sir-Thomas <tp8153@gmail.com>
2025-12-01 22:51:27 -0600

    remove auto login

--- configuration.nix
+++ configuration.nix
@@ -33,8 +33,6 @@
 
   programs.firefox.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
-
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
@@ -52,13 +50,11 @@
     tofi
     git
     pulseaudio
-    brightnessctl
     obsidian
   ];
 
   environment.variables.EDITOR = "nvim";
 
-  services.getty.autologinUser = "tommy";
   services.openssh.enable = true;
   services.tailscale.enable = true;
   services.printing.enable = true;
@@ -69,13 +65,6 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
