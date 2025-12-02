c21cbaf308ff7d48992435a41aea308e0cf1ccf3
Sir-Thomas <tp8153@gmail.com>
2025-12-01 23:01:23 -0600

    fix formatting

--- flake.nix
+++ flake.nix
@@ -1,29 +1,29 @@
 {
-	description = "Tommy's Nixos Configuration";
+  description = "Tommy's Nixos Configuration";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.05";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-		nixosConfigurations.desktop-nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager
-				{
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						users.tommy = import ./home.nix;
-						backupFileExtension = "backup";
-					};
-				}
-			];
-		};
-	};
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.desktop-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.tommy = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,47 +1,47 @@
 { config, pkgs, ... }:
 
 {
-	home.username = "tommy";
-	home.homeDirectory = "/home/tommy";
-	home.stateVersion = "24.05";
-	programs.git = {
-		enable = true;
-		userName = "Sir-Thomas";
-		userEmail = "tp8153@gmail.com";
-		extraConfig.init.defaultBranch = "main";
-	};
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			nrs = "sudo nixos-rebuild switch --flake ~/nixos/";
-		};
-		bashrcExtra = ''
-			if [[ -z "$ZELLIJ" ]]; then
-    				if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
-        				zellij attach -c
-    				else
-        				zellij
-    				fi
+  home.username = "tommy";
+  home.homeDirectory = "/home/tommy";
+  home.stateVersion = "24.05";
+  programs.git = {
+    enable = true;
+    userName = "Sir-Thomas";
+    userEmail = "tp8153@gmail.com";
+    extraConfig.init.defaultBranch = "main";
+  };
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos/";
+    };
+    bashrcExtra = ''
+      if [[ -z "$ZELLIJ" ]]; then
+        if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+          zellij attach -c
+        else
+          zellij
+        fi
 
-    				if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
-        				exit
-    				fi
-			fi
-		'';
-		profileExtra = ''
-			if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-				exec uwsm start -S hyprland-uwsm.desktop
-			fi
-		'';
-	};
-	home.file.".config/hypr".source = ./hypr;
-	home.file.".config/waybar".source = ./waybar;
-	home.file.".config/foot".source = ./foot;
-	home.pointerCursor = {
-		gtk.enable = true;
-		x11.enable = true;
-		package = pkgs.bibata-cursors;
-		name = "Bibata-Modern-Classic"
-		size = 16;
-	};
+        if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+          exit
+        fi
+      fi
+    '';
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec uwsm start -S hyprland-uwsm.desktop
+      fi
+    '';
+  };
+  home.file.".config/hypr".source = ./hypr;
+  home.file.".config/waybar".source = ./waybar;
+  home.file.".config/foot".source = ./foot;
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic"
+    size = 16;
+  };
 }
