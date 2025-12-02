a8ec7a6cca44baefc419676e13993726a2ef05f5
Sir-Thomas <tp8153@gmail.com>
2025-12-01 23:03:57 -0600

    clean up format

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tommy's Nixos Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
