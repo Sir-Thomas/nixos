3c6b33f6c9dc77f8d0dcbd130d064b763964dd8d
Sir-Thomas <tp8153@gmail.com>
2025-12-10 21:52:52 -0600

    fix typo

--- home.nix
+++ home.nix
@@ -41,7 +41,7 @@
     gtk.enable = true;
     x11.enable = true;
     package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic"
+    name = "Bibata-Modern-Classic";
     size = 16;
   };
 }
