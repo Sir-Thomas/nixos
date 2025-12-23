76897121f483e6f34816558447dda529f66a0e1e
Sir-Thomas <tp8153@gmail.com>
2025-12-22 23:15:54 -0600

    add esphome

--- hosts/server/services/homeassistant.nix
+++ hosts/server/services/homeassistant.nix
@@ -1,11 +1,22 @@
 { ... }:
 
 {
-  virtualisation.oci-containers.containers.homeassistant = {
-    image = "ghcr.io/home-assistant/home-assistant:stable";
-    volumes = [ "home-assistant:/config" ];
-    environment.TZ = "America/Chicago";
-    devices = [ "/dev/ttyUSB0:/dev/ttyUSB0" ];
-    extraOptions = [ "--network=host" ];
+  virtualisation.oci-containers.containers = {
+    homeassistant = {
+      image = "ghcr.io/home-assistant/home-assistant:stable";
+      volumes = [ "homeassistant:/config" ];
+      environment.TZ = "America/Chicago";
+      devices = [ "/dev/ttyUSB0:/dev/ttyUSB0" ];
+      extraOptions = [ "--network=host" ];
+    };
+    esphome = {
+      image = "ghcr.io/esphome/esphome:latest";
+      volumes = [
+        "esphome-config:/config"
+	"/etc/localtime:/etc/localtime:ro"
+      ];
+      ports = [ "6052:6052" ];
+    };
   };
+  networking.firewall.allowedTCPPorts = [ 8123 ];
 }
