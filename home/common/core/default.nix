6a9f286c415d1fe9c667928d8efc9df724ce895a
Sir-Thomas <tp8153@gmail.com>
2026-01-01 15:32:23 -0600

    add direnv

--- home/common/core/default.nix
+++ home/common/core/default.nix
@@ -4,21 +4,28 @@
   home.username = "tommy";
   home.homeDirectory = "/home/tommy";
   home.stateVersion = "24.05";
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name = "Sir-Thomas";
-      user.email = "tp8153@gmail.com";
-      init.defaultBranch = "main";
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user.name = "Sir-Thomas";
+        user.email = "tp8153@gmail.com";
+        init.defaultBranch = "main";
+      };
     };
-  };
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake ~/nixos/";
-      gc = "sudo nix-collect-garbage -d";
-      cb = "sudo /run/current-system/bin/switch-to-configuration boot";
-      tofi-fix = "rm ~/.cache/tofi-drun";
+    bash = {
+      enable = true;
+      shellAliases = {
+        nrs = "sudo nixos-rebuild switch --flake ~/nixos/";
+        gc = "sudo nix-collect-garbage -d";
+        cb = "sudo /run/current-system/bin/switch-to-configuration boot";
+        tofi-fix = "rm ~/.cache/tofi-drun";
+      };
+    };
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      nix-direnv.enable = true;
     };
   };
 }
